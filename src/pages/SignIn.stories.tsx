b34c861a4416d7ddec0348171ccb27575ffcdad6
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2022-10-16 23:26:59 -0300

    Aula After

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { SignIn } from "./Signin";
+import { rest } from 'msw';
+
+export default {
+    title: 'Pages/SignIn',
+    component: SignIn,
+    args: {
+    },
+    argTypes: {},
+    parameters: {
+        msw: {
+            handlers: [
+                rest.post('/sessions', (req, res, ctx) => {
+                    return res(ctx.json({
+                        message: "Login Realizado!"
+                    }))
+                })
+            ]
+        }
+    }
+} as Meta
+
+export const Default: StoryObj = {
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        userEvent.type(canvas.getByPlaceholderText('Digite o seu e-mail'), 'diego@rocketseat.com.br');
+        userEvent.type(canvas.getByPlaceholderText('**************'), '123456789');
+
+        userEvent.click(canvas.getByRole('button'));
+
+        await waitFor(() => {
+            return expect(canvas.getByText('Login Realizado!')).toBeInTheDocument()
+        })
+
+    }
+}
